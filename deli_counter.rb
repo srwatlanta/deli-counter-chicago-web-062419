52fade997f38ff9178152f95c12ca2d32349a553
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 23:25:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line (array)
   line_list = []
   if array.length < 1
-    puts "the line is currently empty."
+    puts "The line is currently empty."
   else array.for each do |person, index|
     line_list << "hello #{person} you are number #{index + 1} in line."
   end
